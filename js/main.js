630bd1a730d5fbf94cf84f4499a43df2a5ea52ac
VolodymyrPst <36100885+VolodymyrPst@users.noreply.github.com>
2022-06-21 20:05:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow:'<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
+    })
+
+    $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    })
+
+    $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor:'.slider-map',
+    })
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider',
+        focusOnSelect: true
+    })
+
+    $('.travel__slider').slick({
+        slidesToShow: 1,
+        infinite: true,
+        slidesToScroll: 1,
+        prevArrow:'<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    })
+
+    $('.sleep__slider').slick({
+        slidesToShow: 1,
+        infinite: true,
+        slidesToScroll: 1,
+        prevArrow:'<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    })
+    
+
+  
+});
